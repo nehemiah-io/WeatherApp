aa1053daef1667562a5d6993a6a88c36ad964a7a
Nehemiah Odior <odior@Mamo.local>
2016-11-02 13:59:03 -0700

    First commit

---
+++ Pods/Target Support Files/Pods-MakeWeather-starter/Pods-MakeWeather-starter-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MakeWeather_starter : NSObject
+@end
+@implementation PodsDummy_Pods_MakeWeather_starter
+@end
---
+++ Pods/Target Support Files/Pods-MakeWeather-starter/Pods-MakeWeather-starter-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_MakeWeather_starterVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_MakeWeather_starterVersionString[];
+
